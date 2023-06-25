7a2cda3192f04eda48033aeabb10f001bf81bc6e
Harshit Saini <98768438+GIT-MERCHANT@users.noreply.github.com>
2023-06-26 03:52:23 +0530

    Create Rorr.sol

---
+++ Eth+AVAX/ErrorHandling/Rorr.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Error {
+    
+    uint public Alice=0;
+
+    //Cannot withdraw more than we have
+    function withdraw(uint _i) public   {
+        
+        require(Alice > _i, "Alice does not have enough money");
+        Alice-=_i;
+ 
+    }
+    //Cannot deposit if it results in valur greater than 100
+    function deposit(uint _i) public  returns (string memory) {
+        Alice+=_i;
+        if (Alice > 100) {
+            revert("Alice can not have more than 100");
+        }
+        return "Value decreased, Function called successfully";
+    }
+
+
+    function isempty() public view returns (string memory){
+
+        assert(Alice == 0);
+        return "Yes, Alice has no money";
+    }
+
+
+
+}
