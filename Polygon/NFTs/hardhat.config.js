a54694db99b09195a8a30c0e87c220ea83cd695d
Harshit Saini <98768438+DarthNexus@users.noreply.github.com>
2023-07-24 07:14:11 +0530

    Update hardhat.config.js

--- Polygon/NFTs/hardhat.config.js
+++ Polygon/NFTs/hardhat.config.js
@@ -14,15 +14,15 @@ module.exports = {
   networks: {
     mumbai: {
       url: 'https://rpc-mumbai.maticvigil.com',
-      accounts: ["10a867b51bb123eb52f5129a0cd1613e773d9ef6758e79401120da9f2c1850c7"],
+      accounts: ["10a81850c7"],
     },
     goerli: {
       url: `https://rpc.ankr.com/eth_goerli`,
-      accounts: ["10a867b51bb123eb52f5129a0cd1613e773d9ef6758e79401120da9f2c1850c7"],
+      accounts: ["10a8671250c7"],
     },
     sepolia: {
       url: 'https://ethereum-sepolia.blockpi.network/v1/rpc/public',
-      accounts: ["10a867b51bb123eb52f5129a0cd1613e773d9ef6758e79401120da9f2c1850c7"],
+      accounts: ["10a84011207"],
     },
   },
 };
